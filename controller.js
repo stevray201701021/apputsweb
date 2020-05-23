8443a95c25358e9c54f25ce1ceca15e99aeb792e
ray <stevray98@gmail.com>
2020-05-23 09:07:29 +0700

    Post Montir

--- controller.js
+++ controller.js
@@ -22,11 +22,28 @@ exports.tampilsemuasparepart = function(req,res){
 
 exports.tampilbedasarkanid = function(req,res){
     let id = req.params.id;
-    connection.query('SELECT * FROM t_sparepart where id_sparepart', function(error, rows, fields){
+    connection.query('SELECT * FROM t_sparepart where id_sparepart = ?', function(error, rows, fields){
         if(error){
        console.log(error);
     } else {
         response.ok(rows, res)
     }
     });
+};
+
+//add data montir
+exports.tambahmontir = function(req,res){
+    var id_montir = req.body.id_montir
+    var nama_montir = req.body.nama_montir
+    var iharga_perjam = req.body.harga_perjam
+
+    connection.query('INSERT INTO t_montir (id_montir,nama_montir,harga_perjam) VALUES(?,?,?)'),
+    [id_montir,nama_montir,harga_perjam],
+    function(error,rows,fields){
+    if(error){
+        console.log(error);
+     } else {
+         response.ok(rows, res)
+     }
+    };
 };
